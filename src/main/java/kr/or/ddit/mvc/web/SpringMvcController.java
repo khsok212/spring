907394fc6f4df0e63d3f33009e445b04ce1adf6f
kang <khsok212@gmail.com>
2019-09-19 16:17:39 +0900

    spring mvc  . ModelAndView  . viewResolver    . beanNameViewResolver (jsonView)    . ajax  . customView   . fileDownloadView

--- src/main/java/kr/or/ddit/mvc/web/SpringMvcController.java
+++ src/main/java/kr/or/ddit/mvc/web/SpringMvcController.java
@@ -22,7 +22,9 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import kr.or.ddit.exception.NoFileException;
 import kr.or.ddit.mvc.model.Main;
@@ -39,6 +41,10 @@ import kr.or.ddit.util.model.FileInfo;
 public class SpringMvcController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(SpringMvcController.class);
+	
+	@javax.annotation.Resource(name = "jsonView")
+	private View jsonView;
+	
 	
 	// @RequestMapping이 붙은 메소드가 실행되기전에 @ModelAttribute 메소드가 먼저 실행되고
 	// 해당 메소드가 리턴하는 값을 Model객체에 자동으로 넣어준다
@@ -242,6 +248,42 @@ public class SpringMvcController {
 	@RequestMapping("error500")
 	public String error500() {
 		throw new NullPointerException();
+	}
+	
+	
+	@RequestMapping("jsonView")
+	public String jsonView(Model model) {
+		
+		List<String> rangers = new ArrayList<String>();
+		rangers.add("brown");
+		rangers.add("cony");
+		rangers.add("sally");
+		
+		model.addAttribute("rangers", rangers);
+		
+		return "jsonView";
+	}
+	
+	
+	@RequestMapping("jsonView2")
+	public View jsonView2(Model model) {
+		
+		List<String> rangers = new ArrayList<String>();
+		rangers.add("brown");
+		rangers.add("cony");
+		rangers.add("sally");
+		
+		model.addAttribute("rangers", rangers);
+		
+		return jsonView;
+//		return new MappingJackson2JsonView();
+	}
+	
+	
+	@RequestMapping("fileDownloadView")
+	public String fileDownloadView(Model model, String pictureName) {
+		model.addAttribute("pictureName", pictureName);
+		return "fileDownloadView";
 	}
 	
 	
